32e2092c8b41d4ff0b973cdcf98755318150bbe1
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2022-09-13 10:55:22 +0700

    update map

--- views/section/contact.php
+++ views/section/contact.php
@@ -37,7 +37,7 @@
         <div class="row" data-aos="fade-up" data-aos-delay="100">
 
           <div class="col-lg-6 ">
-            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.1729049274286!2d106.86082449999999!3d-6.2409292999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3a139c977eb%3A0x5eda169af7690329!2sJl.%20KP.Dalam%20IV%20No.16%2C%20RT.6%2FRW.14%2C%20Kb.%20Baru%2C%20Kec.%20Tebet%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2012830!5e0!3m2!1sen!2sid!4v1663000300739!5m2!1sen!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.489463526665!2d106.82357789999999!3d-6.1989715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f41f2cfbf897%3A0xea8298ca42fb21c1!2sPT.%20BUANA%20MANDIRI%20SOLUSINDO!5e0!3m2!1sen!2sid!4v1663041154105!5m2!1sen!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
           </div>
 
           <div class="col-lg-6">
